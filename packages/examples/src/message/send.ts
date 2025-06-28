7b62d73b9e9a6e43a660ae0e1c7cc5e9e434f98a
Wesley F. Young <wesley.f.young@outlook.com>
2025-06-28 23:08:35 +0800

    [examples] change send api example

--- packages/examples/src/message/send.ts
+++ packages/examples/src/message/send.ts
@@ -5,5 +5,5 @@ const group = await bot.getGroup(0); // Substitute with your group's Uin
 
 console.log(await group?.sendMsg(async (b) => {
     b.text('Hello, this is a test message.');
-    await b.image(readFileSync('temp/qrcode.png'));
+    b.image(readFileSync('temp/qrcode.png'));
 }));

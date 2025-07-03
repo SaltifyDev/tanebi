fd98355b91ec2f89f764bcfba0f91d855643bd68
Wesley F. Young <wesley.f.young@outlook.com>
2025-07-03 16:32:24 +0800

    [milky] implement api handler

---
+++ packages/milky/src/api/index.ts
@@ -0,0 +1,72 @@
+import { MilkyApp } from '@/index';
+import z from 'zod';
+
+export interface MilkyApiResponse {
+    status: 'ok' | 'async' | 'failed';
+    retcode: number;
+    data?: unknown;
+    message?: string;
+}
+
+export function Ok(data?: unknown): MilkyApiResponse {
+    return { status: 'ok', retcode: 0, data: data ?? {} };
+}
+
+export function Failed(retcode: number, message: string): MilkyApiResponse {
+    return { status: 'failed', retcode, message };
+}
+
+export interface MilkyApi {
+    endpoint: string;
+    validator: z.ZodType;
+    handler: (app: MilkyApp, payload: unknown) => MilkyApiResponse | Promise<MilkyApiResponse>;
+}
+
+export function defineApi<T extends z.ZodType>(
+    endpoint: string,
+    validator: T,
+    handler: (app: MilkyApp, payload: z.output<T>) => MilkyApiResponse | Promise<MilkyApiResponse>,
+): MilkyApi {
+    return { endpoint, validator, handler };
+}
+
+function encodeZodIssues(issues: z.ZodIssue[]): string {
+    return issues.map((issue) => `[${issue.code}] ${issue.path.join('/')}: ${issue.message}`).join('; ');
+}
+
+export class ApiHandler {
+    readonly apiMap = new Map<string, MilkyApi>();
+
+    constructor(private app: MilkyApp, apiList: MilkyApi[]) {
+        apiList.forEach((api) => {
+            if (this.apiMap.has(api.endpoint)) {
+                throw new Error(`API endpoint "${api.endpoint}" is already defined.`);
+            }
+            this.apiMap.set(api.endpoint, api);
+        });
+    }
+
+    async handle(endpoint: string, payload: unknown): Promise<MilkyApiResponse> {
+        const api = this.apiMap.get(endpoint);
+        if (!api) {
+            return Failed(404, 'API not found');
+        }
+        try {
+            const parsedPayload = api.validator.safeParse(payload);
+            if (!parsedPayload.success) {
+                return Failed(400, 'Invalid payload' + encodeZodIssues(parsedPayload.error.issues));
+            }
+            return await api.handler(this.app, parsedPayload.data);
+        } catch (e) {
+            this.app.logger.warn(
+                `Error while handling API /${endpoint}: ${
+                    e instanceof Error ? e.message + '\n' + e.stack : String(e)
+                }`,
+            );
+            if (e instanceof z.ZodError) {
+                return Failed(400, 'Invalid payload:' + encodeZodIssues(e.issues));
+            }
+            return Failed(500, 'Internal error: ' + (e instanceof Error ? e.message : String(e)));
+        }
+    }
+}

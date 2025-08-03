ce5a19f8834edf654351f884ca1339fb468b27cd
Wesley Young <wesley.f.young@outlook.com>
2025-08-03 08:24:25 +0000

    [milky] simplify sea build script

--- packages/milky/scripts/sea.js
+++ packages/milky/scripts/sea.js
@@ -2,26 +2,10 @@ import { exec } from '@yao-pkg/pkg';
 import process from 'node:process';
 
 const jsEntryName = 'dist/index.cjs';
+const ext = process.platform === 'win32' ? '.exe' : '';
 
-if (process.arch === 'x64') {
-    console.log('Building for Windows x64...');
-    exec([
-        jsEntryName,
-        '-t', 'node20-win-x64',
-        '-o', 'sea/tanebi-milky-win-x64.exe',
-    ]);
-
-    console.log('Building for Linux x64...');
-    exec([
-        jsEntryName,
-        '-t', 'node20-linux-x64',
-        '-o', 'sea/tanebi-milky-linux-x64',
-    ]);
-} else if (process.arch === 'arm64') {
-    console.log('Building for macOS arm64...');
-    exec([
-        jsEntryName,
-        '-t', 'node20-macos-arm64',
-        '-o', 'sea/tanebi-milky-macos-arm64',
-    ]);
-}
+exec([
+    jsEntryName,
+    '-t', 'host',
+    '-o', `sea/tanebi-milky${ext}`,
+]);

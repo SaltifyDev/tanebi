7300e770207021a5da38aed4e666c9244e790906
Wesley F. Young <wesley.f.young@outlook.com>
2025-08-14 10:05:48 +0800

    ci: read github commit hash when building

--- packages/milky/tsup.config.ts
+++ packages/milky/tsup.config.ts
@@ -1,11 +1,25 @@
 import { defineConfig } from 'tsup';
 
+import { execSync } from 'child_process';
+
+function getCommitHash() {
+    try {
+        return execSync('git rev-parse HEAD').toString().trim();
+    } catch (e) {
+        console.error('unable to get commit hash：', e.message);
+        return undefined;
+    }
+}
+
+const COMMIT_HASH = getCommitHash() ?? 'debug';
+console.log(`Using commit hash: ${COMMIT_HASH}`);
+
 export default defineConfig({
     entry: ['src/index.ts'],
     format: 'cjs',
     target: 'node20',
     env: {
-        COMMIT_HASH: process.env.GITHUB_SHA ?? 'debug',
+        COMMIT_HASH,
         BUILD_DATE: new Date().toISOString(),
-    }
+    },
 });

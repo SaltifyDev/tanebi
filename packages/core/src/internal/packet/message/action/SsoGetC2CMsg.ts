e7d4a444df56ddbb10ee94828f36f0c6775a6476
Wesley F. Young <wesley.f.young@outlook.com>
2025-08-15 17:27:43 +0800

    fix(core): proto schema for SsoGetC2CMsgResponse

--- packages/core/src/internal/packet/message/action/SsoGetC2CMsg.ts
+++ packages/core/src/internal/packet/message/action/SsoGetC2CMsg.ts
@@ -1,7 +1,6 @@
 import { ProtoField, ProtoMessage, ScalarType } from '@/internal/util/pb';
 
 export const SsoGetC2CMsg = ProtoMessage.of({
-    peerUin: ProtoField(1, ScalarType.UINT64),
     peerUid: ProtoField(2, ScalarType.STRING),
     startSequence: ProtoField(3, ScalarType.UINT32),
     endSequence: ProtoField(4, ScalarType.UINT32),
@@ -10,7 +9,5 @@ export const SsoGetC2CMsg = ProtoMessage.of({
 export const SsoGetC2CMsgResponse = ProtoMessage.of({
     retcode: ProtoField(1, ScalarType.UINT32),
     errorMsg: ProtoField(2, ScalarType.STRING),
-    startSequence: ProtoField(3, ScalarType.UINT32),
-    endSequence: ProtoField(4, ScalarType.UINT32),
     messages: ProtoField(7, ScalarType.BYTES, false, true),
 });

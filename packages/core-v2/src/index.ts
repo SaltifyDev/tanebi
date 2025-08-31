0c5a264958f1f2e1253417c146fbfd231e5da205
Wesley F. Young <wesley.f.young@outlook.com>
2025-08-31 22:03:56 +0800

    feat(core-v2): init bot context on creating

--- packages/core-v2/src/index.ts
+++ packages/core-v2/src/index.ts
@@ -1,25 +1,32 @@
 import { AppInfo, DeviceInfo, Keystore, SignProvider } from '@/common';
+import { BotContext } from '@/internal';
 import type TypedEventEmitter from 'typed-emitter';
 import { EventEmitter } from 'node:events';
 
+export const ctx = Symbol('Internal context');
 export const emitLog = Symbol('Internal emit log');
 
 export class Bot {
+    //#region Internal Field
+    private readonly [ctx]: BotContext;
     private readonly log = new EventEmitter() as TypedEventEmitter<{
         trace: (moduleName: string, message: string) => void;
         info: (moduleName: string, message: string) => void;
         warning: (moduleName: string, message: string, error?: unknown) => void;
         fatal: (moduleName: string, message: string, error?: unknown) => void;
     }>;
+    //#endregion
 
     constructor(
-        readonly appInfo: AppInfo,
-        readonly deviceInfo: DeviceInfo,
-        readonly keystore: Keystore,
-        readonly signProvider: SignProvider
+        appInfo: AppInfo,
+        deviceInfo: DeviceInfo,
+        keystore: Keystore,
+        signProvider: SignProvider
     ) {
+        this[ctx] = new BotContext(appInfo, deviceInfo, keystore, signProvider);
     }
 
+    //#region Internal API
     [emitLog](
         level: 'trace' | 'info' | 'warning' | 'fatal',
         thisRefOrModuleName: string | object,
@@ -30,7 +37,9 @@ export class Bot {
             typeof thisRefOrModuleName === 'string' ? thisRefOrModuleName : thisRefOrModuleName.constructor.name;
         this.log.emit(level, moduleName, message, error);
     }
+    //#endregion
 
+    //#region Log listeners
     onTrace(listener: (moduleName: string, message: string) => void) {
         this.log.on('trace', listener);
     }
@@ -46,6 +55,7 @@ export class Bot {
     onFatal(listener: (moduleName: string, message: string, error?: unknown) => void) {
         this.log.on('fatal', listener);
     }
+    //#endregion
 }
 
 export * from './common';

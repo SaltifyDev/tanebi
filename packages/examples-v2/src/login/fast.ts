cbf454a19979b3f01b5d5135c49b4f2d04b797f6
Wesley F. Young <wesley.f.young@outlook.com>
2025-09-01 14:09:43 +0800

    feat(examples-v2): add fast login

---
+++ packages/examples-v2/src/login/fast.ts
@@ -0,0 +1,34 @@
+import {
+    Bot,
+    deserializeDeviceInfo,
+    deserializeKeystore,
+    fetchAppInfoFromSignUrl,
+    KeystoreChangeEvent,
+    serializeKeystore,
+    UrlSignProvider,
+} from '@saltify/tanebi';
+import * as fs from 'node:fs';
+
+if (!fs.existsSync('temp/deviceInfo.json') || !fs.existsSync('temp/keystore.json')) {
+    console.error('Please perform QR code login first.');
+    process.exit(1);
+}
+
+const signUrl = 'https://sign.lagrangecore.org/api/sign/30366';
+
+const bot = await Bot.create(
+    await fetchAppInfoFromSignUrl(signUrl),
+    deserializeDeviceInfo(JSON.parse(fs.readFileSync('temp/deviceInfo.json', 'utf-8'))),
+    deserializeKeystore(JSON.parse(fs.readFileSync('temp/keystore.json', 'utf-8'))),
+    UrlSignProvider(signUrl)
+);
+
+bot.subscribe(KeystoreChangeEvent, (event) => {
+    fs.writeFileSync('temp/keystore.json', JSON.stringify(serializeKeystore(event.newKeystore), null, 4));
+    console.log('Keystore saved to temp/keystore.json');
+});
+
+await bot.tryFastLogin();
+console.log('User', bot.uin, 'logged in.');
+
+export default bot;

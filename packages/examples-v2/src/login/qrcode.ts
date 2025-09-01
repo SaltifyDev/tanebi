4a7b049d0029cc495af61c0b53e36736fc6bcf76
Wesley F. Young <wesley.f.young@outlook.com>
2025-09-01 11:55:54 +0800

    feat: add example for v2

---
+++ packages/examples-v2/src/login/qrcode.ts
@@ -0,0 +1,44 @@
+import * as fs from 'node:fs';
+import {
+    Bot,
+    fetchAppInfoFromSignUrl,
+    newDeviceInfo,
+    newKeystore,
+    UrlSignProvider,
+    serializeDeviceInfo,
+    ctx,
+    serializeKeystore,
+    QrCodeGeneratedEvent,
+    KeystoreChangeEvent,
+} from '@saltify/tanebi';
+
+const signUrl = 'https://sign.lagrangecore.org/api/sign/30366';
+
+const bot = await Bot.create(
+    await fetchAppInfoFromSignUrl(signUrl),
+    newDeviceInfo(),
+    newKeystore(),
+    UrlSignProvider(signUrl)
+);
+
+if (!fs.existsSync('temp')) {
+    fs.mkdirSync('temp');
+}
+
+bot.subscribe(QrCodeGeneratedEvent, (event) => {
+    fs.writeFileSync('temp/qrcode.png', event.qrCodePng);
+    console.log('QR code png saved to temp/qrcode.png');
+    console.log('QR code url:', event.qrCodeUrl);
+});
+
+bot.subscribe(KeystoreChangeEvent, (event) => {
+    fs.writeFileSync('temp/keystore.json', JSON.stringify(serializeKeystore(event.newKeystore), null, 4));
+    console.log('Keystore saved to temp/keystore.json');
+});
+
+await bot.qrCodeLogin();
+
+console.log('User', bot.uin, 'logged in.');
+
+fs.writeFileSync('temp/deviceInfo.json', JSON.stringify(serializeDeviceInfo(bot[ctx].deviceInfo), null, 4));
+console.log('Device info saved to temp/deviceInfo.json');

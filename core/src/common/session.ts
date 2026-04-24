c677780b17489b23fda5598e88de165dfe373d50
Wei FeiYang <wesley.f.young@outlook.com>
2026-04-25 02:11:19 +0800

    feat(core): SessionStore initializer

--- core/src/common/session.ts
+++ core/src/common/session.ts
@@ -1,5 +1,7 @@
 import z from 'zod';
 
+import { randomBytes } from 'node:crypto';
+
 export const zBufferAsArray = z.codec(z.array(z.number().int().min(-128).max(127)), z.instanceof(Buffer), {
   decode: (arr) => Buffer.from(arr),
   encode: (buf) => Array.from(buf),
@@ -20,3 +22,19 @@ export const zSessionStore = z.object({
 });
 
 export type SessionStore = z.infer<typeof zSessionStore>;
+
+export function newSessionStore(): SessionStore {
+  return {
+    uin: 0,
+    uid: '',
+    a2: Buffer.alloc(0),
+    d2: Buffer.alloc(0),
+    d2Key: Buffer.alloc(16),
+    tgtgt: Buffer.alloc(0),
+    encryptedA1: Buffer.alloc(0),
+    noPicSig: Buffer.alloc(0),
+    qrSig: Buffer.alloc(0),
+    guid: randomBytes(16),
+    deviceName: `Lagrange-${randomBytes(3).toString('hex')}`,
+  };
+}
